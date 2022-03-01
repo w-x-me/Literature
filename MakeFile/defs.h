066fd1c1e7acb90bbdb52a9f6a7501a94cf718f0
w-x-me <26129689+w-x-me@users.noreply.github.com>
2022-03-01 14:29:31 +0800

    Create defs.h

---
+++ MakeFile/defs.h
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int Main_Call_Defs();
+int Kbd_Call_Defs();
+int Command_Call_Defs();
+int Display_Call_Defs();
+int Insert_Call_Defs();
+int Search_Call_Defs();
+int Files_Call_Defs();
+int Utils_Call_Defs();
