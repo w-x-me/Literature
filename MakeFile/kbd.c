c83fe9a0b64384dd06bd96610de36e89f1ae20bd
w-x-me <26129689+w-x-me@users.noreply.github.com>
2022-03-02 15:06:18 +0800

    Create kbd.c

---
+++ MakeFile/kbd.c
@@ -0,0 +1,12 @@
+#include"def.h"
+#include"command.h"
+int Kbd_Call_Defs()
+{
+  printf("Kbd_Call_Defs\n");
+  return 0;
+}
+int Kbd_Call_Command()
+{
+   printf("Kbd_Call_Command\n");
+  return 0;
+}
